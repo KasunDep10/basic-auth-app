34218a72ffe9bd26be86d5e9d005d30d205e7081
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-01 22:04:51 +0530

    Implement AuthInterceptor class

--- basic-auth-app-backend/src/main/java/lk/ijse/dep10/app/BasicAuthAppApplication.java
+++ basic-auth-app-backend/src/main/java/lk/ijse/dep10/app/BasicAuthAppApplication.java
@@ -1,13 +1,20 @@
 package lk.ijse.dep10.app;
 
+import lk.ijse.dep10.app.interceptor.AuthInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BasicAuthAppApplication {
+public class BasicAuthAppApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(BasicAuthAppApplication.class, args);
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor());
+    }
 }
--- basic-auth-app-backend/src/main/java/lk/ijse/dep10/app/interceptor/AuthInterceptor.java
+++ basic-auth-app-backend/src/main/java/lk/ijse/dep10/app/interceptor/AuthInterceptor.java
@@ -1,9 +1,36 @@
 package lk.ijse.dep10.app.interceptor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
 
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String authorization = request.getHeader("Authorization");
+        if(authorization != null && authorization.startsWith("Basic")){
+            String base64Credentials = authorization.substring(6);
+            String credentials = new String(Base64.getDecoder().decode(base64Credentials));
+            String[] usernamePassword = credentials.split(":");
+            if(usernamePassword.length == 2){
+                String username = usernamePassword[0];
+                String password = usernamePassword[1];
+                System.out.println("username "+ username);
+                System.out.println("password "+ password);
+
+                if(username.equals("root") && password.equals("ijse")){
+                    return true;
+                }
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+    }
 }
